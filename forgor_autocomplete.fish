2f101834a585b725c363249e0ee96f58aac51e4d
Ethan Christensen <ethan.christensen@sdl.usu.edu>
2023-07-21 14:01:13 -0600

    Support descriptions with double colons

--- forgor_autocomplete.fish
+++ forgor_autocomplete.fish
@@ -6,7 +6,7 @@ function __fish_forgor_generate_completions
   set -l subcommands forgor _subcommands
   set -l completions forgor _completions
   for completion in (forgor _completions)
-      set -l cmd_desc (string split "::" $completion)
+      set -l cmd_desc (string split -m 1 "::" $completion)
       set -l cmd $cmd_desc[1]
       set -l desc $cmd_desc[2]
       complete -c forgor -n "__fish_use_subcommand" -f -a $cmd -d $desc
