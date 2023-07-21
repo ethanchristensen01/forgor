17dcb5f8972a03eacb375131f912fc6a9180a1da
Ethan Christensen <ethan.christensen@sdl.usu.edu>
2023-07-21 13:35:39 -0600

    Fixed subcommand getter in forgors

--- forgors/template.fish
+++ forgors/template.fish
@@ -1,12 +1,11 @@
-# set -l subcommand path change-extension '' (status basename)
-
+# set -l subcommand (status basename | path change-extension '')
 # set -a commands $subcommand "Command description"
 
 # function forgor_$subcommand
 #   echo "Testing testing 123!"
 # end
 
-# Future autocomplete support
-# function forgor_complete_$subcommand
-#   complete -c forgor -n "__fish_seen_subcommand_from $subcommand" -f ...
-# end
+# # Future autocomplete support
+# # function forgor_complete_$subcommand
+# #   complete -c forgor -n "__fish_seen_subcommand_from $subcommand" -f ...
+# # end
